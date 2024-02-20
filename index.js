0b3e98cc2833105d2778ea2d24f14277a730896a
AlejandroV <jalejandrov.98@gmail.com>
2024-02-20 16:46:38 -0500

    Adding controllers and routes for CRUD API

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require("express")
 const app = express()
 const Product= require('./models/product.model.js');
+const productRoute=require('./routes/product.route.js');
 const mongoose = require("mongoose");
-require('dotenv').config();
 
+//middleware
+require('dotenv').config();
 app.use(express.json());
 
+// routes
+app.use('/api/products', productRoute);
 
+// testing server
 app.get( '/', (req,res) =>{
     res.send("Hello from Node API Server Updated");
 });
 
-
-app.post('/api/products', async(req,res) =>{
-    try {
-        const product= await Product.create(req.body);
-        res.status(200).json(product);
-    } catch(error){
-        res.status(500).json({message: error.message});
-    };
-
-});
 
 mongoose.connect(`mongodb+srv://jalejandrov98:${process.env.MONGODB}@backenddb.vrhcarl.mongodb.net/Node-API?retryWrites=true&w=majority`)
   .then(() => {
